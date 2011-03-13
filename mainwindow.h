8e9ab38225dbf159bc902022c96f95ee411522de
Alex F <gfreezy@gmail.com>
2011-03-13 22:43:37 +0800

    mainwindow.ui

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTcpSocket>
 
 namespace Ui {
     class MainWindow;
@@ -17,6 +18,7 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QTcpSocket* tcp_socket;
 };
 
 #endif // MAINWINDOW_H
