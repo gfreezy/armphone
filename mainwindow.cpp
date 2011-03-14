1b432802fc4ffcdbb06d38967cb584fa39dd8311
Alex F <gfreezy@gmail.com>
2011-03-14 13:54:58 +0800

    basic code for gui(client side)

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,147 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "audiodevice.h"
+#include <QDataStream>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    remote_ip = "";
+    remote_port = "1500";
+    tcp_socket = new QTcpSocket();
+    ui->status->setVisible(false);
+    connect(tcp_socket, SIGNAL(readyRead()), this, SLOT(ready_to_read()));
+    connect(tcp_socket, SIGNAL(error(QAbstractSocket::SocketError)),
+            this, SLOT(display_socket_error(QAbstractSocket::SocketError)));
+    is_connected = false;
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::ready_to_read()
+{
+    QDataStream in(tcp_socket);
+    QString reply;
+    in >> reply;
+    if(reply == "accept")
+    {
+        start_talking();
+        ui->btn_disconnect->setEnabled(true);
+        ui->status->setText("Talking ...");
+        is_connected = true;
+    }
+    else if(reply == "stop")
+    {
+        stop_talking();
+        ui->btn_dial->setEnabled(true);
+        ui->btn_disconnect->setEnabled(false);
+        ui->status->setVisible(false);
+        ui->remote_ip->setEnabled(true);
+        ui->remote_port->setEnabled(true);
+        is_connected = false;
+    }
+    else
+    {
+        return;
+    }
+
+}
+
+void MainWindow::on_btn_dial_clicked()
+{
+    ui->btn_dial->setEnabled(false);
+    ui->btn_disconnect->setEnabled(true);
+    ui->remote_ip->setEnabled(false);
+    ui->remote_port->setEnabled(false);
+    remote_ip = ui->remote_ip->text();
+    remote_port = ui->remote_port->text();
+    tcp_socket->abort();
+    tcp_socket->connectToHost(remote_ip, remote_port.toInt());
+    if(tcp_socket->waitForConnected())
+    {
+        QDataStream out(tcp_socket);
+        out << "dial request";
+        ui->status->setText("Waitting For Reply");
+        ui->status->setVisible(true);
+    }
+    else
+    {
+        on_btn_disconnect_clicked();
+    }
+
+}
+
+void MainWindow::on_btn_disconnect_clicked()
+{
+    if(is_connected)
+    {
+        QDataStream out(tcp_socket);
+        out << "stop";
+        stop_talking();
+    }
+    tcp_socket->abort();
+    ui->btn_dial->setEnabled(true);
+    ui->btn_disconnect->setEnabled(false);
+    ui->status->setVisible(false);
+    ui->remote_ip->setEnabled(true);
+    ui->remote_port->setEnabled(true);
+}
+
+void MainWindow::start_talking()
+{
+
+}
+
+void MainWindow::stop_talking()
+{
+
+}
+
+void MainWindow::display_socket_error(QAbstractSocket::SocketError socketError)
+{
+    switch (socketError) {
+    case QAbstractSocket::RemoteHostClosedError:
+        break;
+    case QAbstractSocket::HostNotFoundError:
+        QMessageBox::information(this, tr("Arm Phone"),
+                                 tr("The host was not found. Please check the "
+                                    "host name and port settings."));
+        on_btn_disconnect_clicked();
+        break;
+    case QAbstractSocket::ConnectionRefusedError:
+        QMessageBox::information(this, tr("Arm Phone"),
+                                 tr("The connection was refused by the peer. "
+                                    "Make sure the server is running, "
+                                    "and check that the host name and port "
+                                    "settings are correct."));
+        on_btn_disconnect_clicked();
+        break;
+    default:
+        QMessageBox::information(this, tr("Arm Phone"),
+                                 tr("The following error occurred: %1.")
+                                 .arg(tcp_socket->errorString()));
+        on_btn_disconnect_clicked();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- mainwindow.h
+++ mainwindow.h
@@ -17,8 +17,19 @@ public:
     ~MainWindow();
 
 private:
+    void start_talking();
+    void stop_talking();
     Ui::MainWindow *ui;
     QTcpSocket* tcp_socket;
+    QString remote_ip;
+    QString remote_port;
+    bool is_connected;
+
+private slots:
+    void on_btn_disconnect_clicked();
+    void on_btn_dial_clicked();
+    void ready_to_read();
+    void display_socket_error(QAbstractSocket::SocketError);
 };
 
 #endif // MAINWINDOW_H
